33c6ed2f1a3658516b1c71fefb84b33e071bf870
Naeem Paracha <kurians2006@gmail.com>
2019-05-11 14:10:16 +0500

    Initial Commit

---
+++ SampleDesignUI/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SampleDesignUI
+//
+//  Created by MacBook Pro on 11/05/2019.
+//  Copyright © 2019 Naeem Paracha. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SampleDesignUI/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SampleDesignUI
+//
+//  Created by MacBook Pro on 11/05/2019.
+//  Copyright © 2019 Naeem Paracha. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
